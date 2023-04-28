ba81e4f8dbff1686ca0fdef65609e085b71c648f
sjun <sammanone@naver.com>
2023-04-28 20:13:13 +0900

    커밋 메시지

--- apps/controller/controller.py
+++ apps/controller/controller.py
@@ -6,6 +6,17 @@ import apps.service.few_shot_test as few_shot_test
 import apps.service.image_decode as img_dc
 import apps.service.get_url as gurl
 
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+from PIL import Image
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
 bp = Blueprint(name='bp',
                        import_name=__name__,
                        url_prefix='/fsl')
@@ -15,21 +26,21 @@ def get_test_result():
     json_data = request.get_json()
 
     img = img_dc.image_decoding(json_data) # 이미지 디코딩
-    #result = few_shot_test.test_code(img) # 쿼리 이미지 넣어서 테스트 돌리기
+    result = few_shot_test.find_class(img) # 쿼리 이미지 넣어서 테스트 돌리기
     purl = gurl.get_product_url(result)
 
     return purl
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+# ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@bp.route('/images/<path:path>')
-def send_image(path):
-    if allowed_file(path):
-        print(path)
-        return send_from_directory('static/images', path)
-    else:
-        return 'Invalid file type'
+# @bp.route('/images/<path:path>')
+# def send_image(path):
+#     if allowed_file(path):
+#         print(path)
+#         return send_from_directory('static/images', path)
+#     else:
+#         return 'Invalid file type'
