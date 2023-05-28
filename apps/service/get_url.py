2415f6262a2f2d280d0604dd6c8449c29e26b46e
sjun <sammanone@naver.com>
2023-05-28 14:15:34 +0900

    모르는클래스맞추기

--- apps/service/get_url.py
+++ apps/service/get_url.py
@@ -2,7 +2,7 @@ import json
 
 def get_product_url(test_result):
     file_path = "./apps/resource/product_url.json"
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
         return data[str(test_result)]
